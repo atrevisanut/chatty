ee83c0520d6b8b2a6129a8b03bed948a662bbdd6
atrevisanut <atrevisanut@gmail.com>
2017-05-09 12:34:58 -0600

    added extras

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 const messages = [
 	{
 	  message: "Hi there!",
+      username: "basicusr",
       image: "https://images.unsplash.com/photo-1459664018906-085c36f472af?dpr=1.25&auto=format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop=&bg=",
 	  time: "11:11am"
     }
@@ -29,12 +30,13 @@ app.post('/', function(req, res) {
     var date = new Date(),
         h = date.getHours(),
         m = date.getMinutes(),
+        mm = m.toString().length === 1 ? "0" + m : m,
         ampm = h >= 12 ? 'pm' : 'am';
 	messages.push({
 		message: req.body.message,
         username: req.body.username,
         image: "https://images.unsplash.com/photo-1459664018906-085c36f472af?dpr=1.25&auto=format&fit=crop&w=1199&h=799&q=80&cs=tinysrgb&crop=&bg=",
-		time: h+":"+m+" "+ampm
+		time: h+":"+mm+" "+ampm
 	});
 	res.status(200).set({
 	    'Content-Type': 'application/json',
